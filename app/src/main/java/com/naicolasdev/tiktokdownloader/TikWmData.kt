b7e816043544382b587ec20c2ff01bc4f6f572a4
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-28 20:34:01 -0300

    feat(data): add TikWM API data models

---
+++ app/src/main/java/com/naicolasdev/tiktokdownloader/TikWmData.kt
@@ -0,0 +1,22 @@
+package com.naicolasdev.tiktokdownloader
+
+import com.google.gson.annotations.SerializedName
+
+data class TikWmResponse(
+    @SerializedName("code") val code: Int,
+    @SerializedName("msg") val msg: String,
+    @SerializedName("data") val data: TikWmData?
+)
+
+data class TikWmData(
+    @SerializedName("id") val id: String?,
+    @SerializedName("title") val title: String?,
+    @SerializedName("cover") val cover: String?,
+    @SerializedName("play") val play: String?,
+    @SerializedName("author") val author: TikWmAuthor?
+)
+
+data class TikWmAuthor(
+    @SerializedName("nickname") val nickname: String?,
+    @SerializedName("avatar") val avatar: String?
+)
