aa96b548e3e7ec62b5ad378899003def2e702a63
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-29 11:35:33 -0300

    chore(release): bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.naicolasdev.tiktokdownloader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
