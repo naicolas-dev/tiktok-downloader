909802080c742b22426c3fe16822d4936677ab80
Nicolas Alves <naicolas.dev@gmail.com>
2026-02-20 17:19:06 -0300

    update app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,15 +12,15 @@ android {
         applicationId = "com.naicolasdev.tiktokdownloader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
